c1659e9571bab6e294e8269877a58e3fa4ec7383
nhdrasel <nhdrasel@gmail.com>
2022-03-21 00:27:41 +1300

    Array

---
+++ ArrayLoop/ArrayList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+ArrayList a = new ArrayList();
+a.Add("hello");
+a.Add("bye");
+a.Add("Nahid");
+a.Add("Apple");
+
+Console.WriteLine(a[1]);
+
+foreach (String item in a)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine(a.Contains("Nahid"));
+Console.WriteLine("After Sorting");
+a.Sort();
+
+foreach (String item in a)
+{
+    Console.WriteLine(item);
+}
+
+
+ArrayList Mahmud = new ArrayList();
+Mahmud.Add("Mahmud 0");
+Mahmud.Add("Rasel");
+
+Console.WriteLine(Mahmud[0]);
+
+ArrayList nahid = new ArrayList { "Mahmud", "Rasel" };
+foreach (String print in nahid)
+{
+    Console.WriteLine(print);
+}
+
+Console.WriteLine(nahid.Contains("Rasel")); //True or False
---
+++ ArrayLoop/ThisClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SeleniumCS
+{
+    public class ThisClass
+    {
+        string name;
+        string lastname;
+
+        //method default constructir
+        public ThisClass(String name)
+        {
+            this.name = name;
+
+        }
+
+        public ThisClass(String firstname, String lastname)
+        {
+            this.lastname = lastname;
+
+        }
+        public void getName()
+        {
+            Console.WriteLine("My name is " + this.name);
+        }
+        public void getData()
+        {
+            Console.WriteLine("I am inside the method");
+        }
+
+        static void Main(string[] args)
+        {
+            ThisClass p = new ThisClass("Nahid"); //it's going to constructor
+            // constructor will send name to global variable name
+
+            ThisClass p1 = new ThisClass("Nahid", "Mahmud");
+
+            Console.WriteLine("Second constructor executed ");
+        }
+    }
+}
